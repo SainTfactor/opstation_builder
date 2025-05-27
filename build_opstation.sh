7d3f265a2780df3723ad3c21fdad47d100869302
tyler <sta0003@auburn.edu>
2025-05-26 21:30:22 -0400

    Bugfixes and updates to the build script

--- build_opstation.sh
+++ build_opstation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-ansible-playbook -i $1, site.yml
+if [ "$1" == "" ]; then
+  echo "No target detected.  Please specify target in the form of user@target-ip."
+  exit 0
+fi
+
+user=$(echo "$1" | cut -d@ -f1)
+target=$(echo "$1" | cut -d@ -f2)
+
+ansible-playbook -e "ansible_user=$user" --ask-pass --ask-become-pass -i $target, site.yml
 
